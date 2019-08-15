e5b1b552589831d50ccadd44b07e50179d52f881
Larry <tgfc045@tgfc.tw>
2019-08-15 17:47:48 +0800

    first commit

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping("/getUser")
+    @Cacheable(value="user-key123")
+    public User getUser() {
+        User user=new User("aa@126.com", "aa", "aa123456", "aa","123");
+        System.out.println("若下面没出现“无缓存的时候调用”字样且能打印出数据表示测试成功");
+        return user;
+    }
+}
